df1b11a8fb980debb18d9684a21b252de9ee384e
walaa-Ashyqa <69903479+walaa-Ashyqa@users.noreply.github.com>
2021-06-17 16:35:51 +0300

    Add files via upload

---
+++ js/country.js
@@ -0,0 +1,33 @@
+const xhttp = new XMLHttpRequest();
+const select = document.getElementById("countries");
+const flag = document.getElementById("flag");
+
+let countries;
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        countries = JSON.parse(xhttp.responseText);
+        assignValues();
+        handleCountryChange();
+    }
+};
+xhttp.open("GET", "https://restcountries.eu/rest/v2/all", true);
+xhttp.send();
+
+function assignValues() {
+    countries.forEach(country => {
+        const option = document.createElement("option");
+        option.value = country.alpha2Code;
+        option.textContent = country.name;
+        select.appendChild(option);
+    });
+}
+
+function handleCountryChange() {
+    const countryData = countries.find(
+        country => select.value === country.alpha2Code
+    );
+    flag.style.backgroundImage = `url(${countryData.flag})`;
+}
+
+select.addEventListener("change", handleCountryChange.bind(this));
